806681569bea092402d7f5e3d258737208960110
Alexander Karlsson <alexander.karlsson@learnet.se>
2021-09-17 11:20:14 +0200

    commit nr3

--- scenes.js
+++ scenes.js
@@ -13,7 +13,7 @@ let scenes = {
         ]
     },
     letter_pickup: {
-        info: "The envelope is very small, smaller than any old letter but due to it being thicker it seems like it has alot of contents in it. There's a note attached which reads “If you want to make a change to the world, go to the address on the back of this note. Be sure to not open it. A reward will be awarded to you if you succeed”. You carefully remove the note and turn it around to see both an address and picture. It’s far away from your home, somewhere in the abandoned parts of town, and it would take some time to get there. But then you’re likely to miss work for a few days, and it’ll likely mean you’ll be fired. You ponder for a moment.",
+        info: "The envelope is very small, smaller than any old envelopes, but due to it being thicker it seems like it has alot of contents in it. It's also has a seal over it. There's a note attached which reads “If you want to make a change to the world, go to the address on the back of this note. Be sure to not open it. A reward will be awarded to you if you succeed”. You carefully remove the note and turn it around to see both an address and picture. It’s far away from your home, somewhere in the abandoned parts of town, and it would take some time to get there. But then you’re likely to miss work for a few days, and it’ll likely mean you’ll be fired. You ponder for a moment.",
         selections: [
             {
                 text: "Don't follow the instructions",
@@ -41,7 +41,7 @@ let scenes = {
                     message: "You tell them about the letter you got today. They both look at you with interest and ask to see it. You go in, take it and give it to them. They thank you for helping and leave. The next day you hear you’re getting promoted. You’re a bit surprised as you weren’t the most qualified for a promotion but you took it. Your life has improved since then, but the world’s beyond saving at this point so you’ll live the best life you’ve ever had until the end of the world."
                 }
             },
-            
+
         ]
     },
     Dont_tell: {
@@ -96,7 +96,7 @@ let scenes = {
                 text: "Open door 2",
                 action: {
                     type: "move forward",
-                   target: "choiceA"
+                    target: "choiceA"
                 }
             }
         ]
@@ -156,7 +156,7 @@ let scenes = {
                     message: "You got robbed and beaten by a group of thugs."
                 }
             }
-            
+
         ]
     },
     move_on: {
@@ -165,8 +165,8 @@ let scenes = {
             {
                 text: "Finally arrive",
                 action: {
-                    type: "good end",
-                    message: "You reached the end"
+                    type: "move forward",
+                    target: "arrival"
                 }
             }
         ]
@@ -178,7 +178,7 @@ let scenes = {
                 text: "Hit thug B",
                 action: {
                     type: "move forward",
-                   target: "correct_hit"
+                    target: "correct_hit"
                 }
             },
             {
@@ -188,7 +188,7 @@ let scenes = {
                     message: "Your hits are ineffective towards the thug and they hit you back, knocking you out."
                 }
             }
-            
+
         ]
     },
     correct_hit: {
@@ -205,7 +205,7 @@ let scenes = {
                 text: "Escape",
                 action: {
                     type: "move forward",
-                   target: "escape"
+                    target: "escape"
                 }
             }
         ]
@@ -216,27 +216,121 @@ let scenes = {
             {
                 text: "Finally arrive",
                 action: {
-                    type: "good end",
-                    message: "You reached the end"
+                    type: "move forward",
+                    target: "arrival"
                 }
             }
         ]
     },
     on_foot1: {
-        info: "Your hit knocked the wind out of them. The other's stunned for a moment. You could possibly escape but you're not sure that you should.",
+        info: "You decide to walk. A few hours in",
         selections: [
             {
-                text: "Continue fighting",
+                text: "Choice 1a",
                 action: {
-                    type: "bad end",
-                    message: "You continue fighting but one of them hit you and you fall on the ground and one of them takes your bag before they beat you to death."
+                    
+                    type: "move forward",
+                    target: "on_foot3"
                 }
             },
             {
-                text: "Escape",
+                text: "choice 1b",
+                action: {
+                    type: "bad end",
+                    message: "Bad end"
+                }
+            }
+        ]
+    },
+    on_foot3: {
+        info: "choice a or b nr2",
+        selections: [
+            {
+                text: "Choice 2a",
+                action: {
+                    type: "bad end",
+                    message: "Bad end"
+                }
+            },
+            {
+                text: "choice 2b",
                 action: {
                     type: "move forward",
-                   target: "escape"
+                    target: "on_foot4"
+                }
+            }
+        ]
+    },
+    on_foot4: {
+        info: "choice a or b nr3",
+        selections: [
+            {
+                text: "Choice 3a",
+                action: {
+                    type: "bad end",
+                    message: "Bad end"
+                }
+            },
+            {
+                text: "choice 3b",
+                action: {
+                    type: "move forward",
+                    target: "on_foot5"
+                }
+            }
+        ]
+    },
+    on_foot5: {
+        info: "choice a or b nr4",
+        selections: [
+            {
+                text: "Choice 4a",
+                action: {
+                    type: "move forward",
+                    target: "on_foot6"
+                }
+            },
+            {
+                text: "choice 4b",
+                action: {
+                    type: "bad end",
+                    message: "Bad end"
+                }
+            }
+        ]
+    },
+    on_foot6: {
+        info: "Your are almost at your destination.",
+        selections: [
+            {
+                text: "Finally arive",
+                action: {
+                    type: "move forward",
+                    target: "arrival"
+                }
+            }
+        ]
+    },
+    arrival: {
+        info: "After finally looking for hours you find the building the address belonged to. You look at the picture on the note to be sure. It’s an old abandoned warehouse. Nothing that stands out. Trying to locate the nearest entrance to get inside you find a closed door. When you try to turn the door handle you find that it’s stuck. A bit strange as it looks abandoned. You look around for anything to pry the door open. But you don’t find anything. “Looking for anything?” you hear from your right. Looking towards the direction of the voice you see what looks like three people standing in front of the big warehouse door. You answer that yes, you were looking for something. They look at each other then move towards you. The one in front asks what this something you’re looking for is. When they see the note in your hand they ask to see it. You are a bit nervous when you show them the note. You see their eyes light up then they ask you to follow them. They take out a key and open the door. Nervously following them you all go into the abandoned warehouse. There are more people in the warehouse. Some are armed, some are not. Some are wearing armbands, and when you look closer you see the symbol of the resistance. Someone, who looks like a scientist, comes up to you holding a picture. ”Do you have an envelope like this with you?” they ask. The envelope in the picture looks just like the one you have. You take out the envelope from your backpack and give it to them. Everyone's faces light up when they see you pull out the letter. The scientists carefully break the seal and check the contents. The room's atmosphere is filled with eager anticipation and the only sounds that can be heard are the ones of ruffling paper. “It’s legit” are the words that break the silence. People start cheering. The scientist comes and thanks you for delivering the envelope. They see the puzzlement from your face. “You probably have some questions, do you?”.",
+        selections: [
+            {
+                text: "Time for some answers",
+                action: {
+                    type: "move forward",
+                    target: "question_time"
+                }
+            }
+        ]
+    },
+    question_time: {
+        info: "‘We’ve fought the government for a while. While my comrades are fighting with weapons, I and my fellow scientists fight using science. Anything to help the earth and the people, while also potentially decreasing the profits of those in charge of the government. It has been a stalemate for a while. Until recently that is. A recent discovery could not only help the environment and people but also may be the thing we need to have a chance. But the government tries to stop us any way they can so it’s very risky to send and spread the data without it being intercepted. But that’s where you come in.’ he points at the opened envelope. ‘That envelope is one of thousands of others and it has everything we need to put this discovery into effect. It was sent to people who were likely to forward it. We of course didn’t expect everyone to do it and we also expected some to give it to the police. But you’re currently the first one, but hopefully not the last, to have made it. Now, as for your reward.’ He pulls out a cheque with an amount of money that you could live off of for a year or two. ‘Here, take it.’ You take the cheque and place it in your jacket. ‘ You could leave right now, but due to you having taken this journey I got a proposition for you. Would you like to join the resistance? We need everyone we can muster to help our cause. So do you want to help build a better tomorrow?’ He puts his hand out, as if to want to shake your hand. After going over what you’ve been through in your mind, you shake his hand and respond.",
+        selections: [
+            {
+                text: "For a better tomorrow!",
+                action: {
+                    type: "good end",
+                    message: "You reached the end, but why not try a different route?"
                 }
             }
         ]
